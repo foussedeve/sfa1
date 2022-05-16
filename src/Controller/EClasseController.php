6f897e507a7ff9849a5b7d7e2061d6a247288159
foussedeve <foussedev@gmail.com>
2022-05-16 15:55:28 +0200

    add (add and edit) functionality for classe entity

--- src/Controller/EClasseController.php
+++ src/Controller/EClasseController.php
@@ -4,31 +4,57 @@ namespace App\Controller;
 
 use App\Entity\Classe;
 use App\Form\ClasseType;
+use App\Repository\ClasseRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EClasseController extends AbstractController
 {
+    private $em;
+    public function __construct(ManagerRegistry $manager)
+    {
+        $this->em = $manager->getManager();
+    }
+
     /**
      * @Route("/classes", name="ecole_classe")
      */
-    public function index(): Response
+    public function index(ClasseRepository $classeRepo): Response
     {
+        $classes = $classeRepo->findAll();
         return $this->render('e_classe/index.html.twig', [
-            'controller_name' => 'EClasseController',
+            'classes' => $classes,
         ]);
     }
 
     /**
      * @Route("/classe/add", name="add_classe")
+     * @Route("/classe/edit/{id}", name="edit_classe")
      */
-    public function addClasse(): Response
-    {$classe=new Classe();
-        $classeForm=$this->createForm(ClasseType::class,$classe);
+    public function addClasse(Request $request, classe $classe = null): Response
+    {
+        if ($classe == null) {
+            $classe = new Classe();
+        }
+
+        $form = $this->createForm(ClasseType::class, $classe);
+
+        // request traitement
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->em->persist($classe);
+            $this->em->flush();
+            return $this->redirectToRoute("ecole_classe");
+        }
 
         return $this->renderForm('e_classe/add.html.twig', [
-           "form"=>$classeForm
+            "form" => $form,
+            "editMode" => $classe->getId() == null,
+            "title" => $classe->getId() == null ? "Ajouter une nouvelle Classe" : "Modifier la classe"
         ]);
     }
 }
